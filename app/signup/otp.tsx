d42d62f1b9e606e2791d1d5a49490d637039a8f1
HSangwaiya <ashsangwaiya@gmail.com>
2025-06-12 13:07:47 +0530

    minor changes

--- app/signup/otp.tsx
+++ app/signup/otp.tsx
@@ -13,7 +13,7 @@ import BackgroundGradient from '@/components/BackgroundGradient';
 import BackgroundEffects from '@/components/BackgroundEffects';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ActivityIndicator } from 'react-native';
-import { getHash, startOtpListener, removeListener } from 'react-native-otp-verify';
+// import { getHash, startOtpListener, removeListener } from 'react-native-otp-verify';
 
 export async function storeUserId(userId: string) {
   await SecureStore.setItemAsync('userId', userId);
@@ -30,32 +30,32 @@ export default function OtpScreen() {
   const inputRefs = useRef<(TextInput | null)[]>([]);
   const glowAnimation = new Animated.Value(0);
 
-  useEffect(() => {
-    if (Platform.OS === 'ios') return;
-    let listener: any;
+  // useEffect(() => {
+  //   if (Platform.OS === 'ios') return;
+  //   let listener: any;
 
-    // Step 1: get hash for SMS formatting
-    getHash()
-      .then(hashes => console.log('App hash:', hashes[0]))
-      .catch(console.error);
+  //   // Step 1: get hash for SMS formatting
+  //   getHash()
+  //     .then(hashes => console.log('App hash:', hashes[0]))
+  //     .catch(console.error);
 
-    // Step 2: start listening for SMS containing OTP
-    startOtpListener((message: string) => {
-      console.log('Received SMS:', message);
-      const match = message.match(/(\d{4})/);
-      if (match) {
-        const digits = match[1].split('');
-        setOtp(digits);
-        removeListener();  // stop listening after capture
-      }
-    })
-      .catch(console.error);
+  //   // Step 2: start listening for SMS containing OTP
+  //   startOtpListener((message: string) => {
+  //     console.log('Received SMS:', message);
+  //     const match = message.match(/(\d{4})/);
+  //     if (match) {
+  //       const digits = match[1].split('');
+  //       setOtp(digits);
+  //       removeListener();  // stop listening after capture
+  //     }
+  //   })
+  //     .catch(console.error);
 
-    // Step 3: cleanup on unmount
-    return () => {
-      removeListener();
-    };
-  }, []);
+  //   // Step 3: cleanup on unmount
+  //   return () => {
+  //     removeListener();
+  //   };
+  // }, []);
 
   useEffect(() => {
     const startGlowAnimation = () => {
